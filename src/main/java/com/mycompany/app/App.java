054ed908572af970de5768491fc2cc3c4fc44386
youssef-khalifi <youssefkhalifi767@gmail.com>
2024-05-23 13:24:11 +0000

    ajouter un nouveau message sur class App

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Bonjour et bon courage dans votre examen en DevOps" );
+	System.out.println( "bon courage pour Jenkins" );
     }
 }
